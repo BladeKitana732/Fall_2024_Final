e28416e828bd9a4d893abd86c2be601f283af440
BladeKitana732 <dev.malave7@gmail.com>
2024-11-14 09:04:09 -0600

    adding some more pieces of what is required ensuring there is a dictionary for easy data extraction

--- faculty.py
+++ faculty.py
@@ -5,8 +5,10 @@ class Faculty:
     def __init__(self, first_name, last_name, courses):
         first_name = self.first_name
         last_name = self.last_name 
-        courses = self.courses 
+        courses = []
 
     #method 
-    def assign_course(course):
+    def assign_course(course): #also will need a dictionary here for key pair value of teacher name to course 
         print("test")
+
+    
