b6ac201156d6625e5c4356d84e9e22406c7c0e30
BladeKitana732 <dev.malave7@gmail.com>
2024-11-19 11:40:24 -0600

    adding functions directions to course module

--- course.py
+++ course.py
@@ -1,10 +1,16 @@
 #courses class 
 class Course: 
     #attributes 
-    def __init__(self, course_name, faculty):
-        course_name = self.course_name
+    def __init__(self, title , faculty):
+        title = self.title
         faculty = None #was advised on requirements to set this to None initially 
 
     #methods 
     def get_info(self): #needed to self to return information about course and its assigned faculy; still need a dictionary 
-        pass
+        #returns info about the course and assigned faculty 
+        faculty_name = f"{self.faculty.first_name} {self.faculty.last_name}" if self.faculty else "None"
+        return {"Course Name:" : self.title, "Faculty:" : faculty_name}
+    
+    def __str__(self):
+        #returns a string representation of Course instance 
+        pass 
