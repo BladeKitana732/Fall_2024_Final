b11bea1948b61d1c4b87400b5e40f7ecd36288d8
BladeKitana732 <dev.malave7@gmail.com>
2024-11-19 21:08:18 -0600

    updating and now working on implementing the updates made from previous module updates to the university module then finally finish in main module

--- university_system.py
+++ university_system.py
@@ -12,15 +12,17 @@ class UniversitySystem:
         courses = []
 
     #methods 
-    def add_student(first_name, last_name): 
+    def add_student(self, first_name, last_name): 
+        #adds a student object to the list of students 
+        student = Student(self, first_name = str, last_name = str):
+        self.students.append(student)
+
+    def add_faculty(self, first_name, last_name):
         print("test")
 
-    def add_faculty(first_name, last_name):
+    def add_course(self, course_name):
         print("test")
 
-    def add_course(course_name):
-        print("test")
-
-    def save_students_to_csv(filename = "students.csv"):
+    def save_students_to_csv(self, filename = "students.csv"):
         print("test")
 
